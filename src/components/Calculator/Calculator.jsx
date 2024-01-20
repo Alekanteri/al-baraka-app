9d70c169919d20d083ca2ac4b6b922ed39020fc8
Alekanteri <sandor.basc@gmail.com>
2024-01-20 18:09:28 +0300

    calculator upgrade

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,89 +1,159 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import css from "./Calculator.module.scss";
 import "../../styles/global.scss";
 
 const Calculator = () => {
-  const [count, setCount] = useState(8000);
-  const [month, setMonth] = useState(2);
+  const [count, setCount] = useState(50000);
+  const [month, setMonth] = useState(3);
   const [firstPayment, setFistPayment] = useState(0);
   const [monthPayment, setMonthPayment] = useState(0);
-  const [murkUp, setMurkUp] = useState(0);
+  const [markUp, setMarkUp] = useState(0);
   const [result, setResult] = useState(0);
-  const [discount, setDiscount] = useState(true);
+  const [discount, setDiscount] = useState(0.02);
+  const [countInput, setCountInput] = useState(count);
+  const [monthInput, setMonthInput] = useState(month);
 
-  const firstPaymentCalculation = async () => {
-    const firstPaymentcalc = (count / 100) * 30;
-    setFistPayment(firstPaymentcalc);
+  const handleChangeCount = (event) => {
+    document.getElementById("inputCountId").value = event.target.value;
+    setCountInput(event.target.value);
+    setCount(event.target.value);
+    calculateAll(event.target.value, month, discount);
   };
 
-  const resultCalculation = async () => {
-    const resultCalc = count + (count / 100) * (discount ? 2 : 3 * month);
-    setResult(resultCalc);
+  const handleChangeMonth = (event) => {
+    document.getElementById("inputMonthId").value = event.target.value;
+    setMonthInput(event.target.value);
+    setMonth(event.target.value);
+    calculateAll(count, event.target.value, discount);
   };
 
-  const monthPaymentCalculation = async () => {
-    const monthPaymentcalc = (result - firstPayment) / 2;
-    setMonthPayment(monthPaymentcalc);
+  const handleInputCount = (event) => {
+    if (event.key === "Enter") {
+      if (event.target.value > 200000) {
+        event.target.value = 200000;
+        setCount(200000);
+      } else if (event.target.value < 8000) {
+        event.target.value = 8000;
+        setCount(8000);
+      }
+      setCountInput(event.target.value);
+      setCount(event.target.value);
+      calculateAll(event.target.value, month, discount);
+    }
   };
 
-  const murkUpCalculation = async () => {
-    const murkUpcalc = result - count;
-    setMurkUp(murkUpcalc);
+  const handleInputMonth = (event) => {
+    if (event.key === "Enter") {
+      if (event.target.value > 12) {
+        event.target.value = 12;
+        setMonth(12);
+      } else if (event.target.value < 2) {
+        event.target.value = 2;
+        setMonth(2);
+      }
+      setMonthInput(event.target.value);
+      setMonth(event.target.value);
+      calculateAll(count, event.target.value, discount);
+    }
   };
 
-  const func = async () => {
-    await resultCalculation();
-    await firstPaymentCalculation();
-    await monthPaymentCalculation();
-    await murkUpCalculation();
+  const calculateAll = (newCount, newMonth, newDiscount) => {
+    let first = Math.round(newCount * 0.3);
+    first = first / 100;
+    first = Math.ceil(first);
+    first = first * 100;
+    let markUpPayment = Math.round(
+      newDiscount * Number(newMonth) * Number(newCount)
+    );
+    let totalPayment = Math.round(Number(newCount) + markUpPayment);
+    totalPayment = totalPayment / 100;
+    totalPayment = Math.ceil(totalPayment);
+    totalPayment = totalPayment * 100;
+    let sumLeft = totalPayment - first;
+    let paymentPerMonth = Math.round(sumLeft / Number(newMonth));
+
+    paymentPerMonth = paymentPerMonth / 100;
+    paymentPerMonth = Math.ceil(paymentPerMonth);
+    paymentPerMonth = paymentPerMonth * 100;
+    totalPayment = paymentPerMonth * Number(newMonth) + first;
+    markUpPayment = totalPayment - Number(newCount);
+
+    setFistPayment(first);
+    setMarkUp(markUpPayment);
+    setResult(totalPayment);
+    setMonthPayment(paymentPerMonth);
   };
+
+  useEffect(() => {
+    calculateAll(count, month, discount);
+  }, []);
 
   return (
     <>
       <div className={css.calculationContainer}>
         <h1 className={css.title}>Рассчитайте свою персональную рассрочку</h1>
         <div className={css.calculatorBody}>
-          <form onChange={func}>
+          <div className={css.formContainer}>
             <div className={css.discountBody}>
               <div
                 className={`${css.discountBtn} ${
-                  discount ? css.activeBtn : ""
+                  discount == 0.02 ? css.activeBtn : ""
                 }`}
                 onClick={() => {
-                  setDiscount(true);
+                  setDiscount(0.02);
+                  calculateAll(count, month, 0.02);
                 }}
               >
                 Идеал
               </div>
               <div
                 className={`${css.discountBtn} ${
-                  !discount ? css.activeBtn : ""
+                  discount == 0.03 ? css.activeBtn : ""
                 }`}
                 onClick={() => {
-                  setDiscount(false);
+                  setDiscount(0.03);
+                  calculateAll(count, month, 0.03);
                 }}
               >
                 Стандарт
               </div>
             </div>
-            <label>Стоимость товара: {count}</label>
+            <label>Стоимость товара:</label>
+            <input
+              type="number"
+              id="inputCountId"
+              min={8000}
+              max={200000}
+              defaultValue={countInput}
+              onKeyDown={handleInputCount}
+            />
             <input
               type="range"
-              min={8}
-              max={200}
-              defaultValue={8}
-              onChange={(e) => setCount(e.target.value * 1000)}
+              min={8000}
+              max={200000}
+              step={100}
+              value={count}
+              onChange={handleChangeCount}
             />
             <br />
-            <label>Срок рассрочки: {month}</label>
+            <label>Срок рассрочки:</label>
+            <input
+              type="number"
+              id="inputMonthId"
+              min={2}
+              max={12}
+              defaultValue={monthInput}
+              onKeyDown={handleInputMonth}
+            />
             <input
               type="range"
               min={2}
               max={12}
-              defaultValue={2}
-              onChange={(e) => setMonth(e.target.value)}
+              step={1}
+              value={month}
+              onChange={handleChangeMonth}
             />
-          </form>
+          </div>
           <div className={css.calculatorResult}>
             <div>
               Общая стоимость
@@ -94,7 +164,7 @@ const Calculator = () => {
             <p>Первый платеж</p>
             <p>{firstPayment}</p>
             <p>Торговая наценка</p>
-            <p>{murkUp}</p>
+            <p>{markUp}</p>
           </div>
         </div>
       </div>
